4afd9bfdcc419f5285233ebd25bf59ab83970a0d
Mark Sample <samplereality@gmail.com>
2022-07-24 08:40:58 -0400

    Changed max zoom level

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ let rg = RiTa.grammar(rules);
 
 var map = L.map('map', {
     center: [35.227, -80.8431],
-    zoom: 6
+    zoom: 5
 });
 
 // Set bounds for the map
@@ -25,7 +25,7 @@ map.on('drag', function() {
 L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
-	minZoom: 1,
+	minZoom: 3,
 	maxZoom: 16,
 	ext: 'jpg'
 }).addTo(map);
