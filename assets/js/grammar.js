37afbc065b30a3cff46417ab3243f117357aa160
Mark Sample <samplereality@gmail.com>
2022-07-24 08:51:18 -0400

    Starting to build out grammar

--- assets/js/grammar.js
+++ assets/js/grammar.js
@@ -1,5 +1,5 @@
 var rules = {
-    start: "$negative | $positive",
+    start: "$fire | $heat| $flood | $drought | $negative | $positive",
     positive: "$pos_emotion $subject $future $pos_status.",
     negative: "$neg_emotion $subject $predicate $status.",
     neg_emotion: "Gutted. | Melancholia. | A deep well of sadness. | Despairing.",
@@ -8,5 +8,9 @@ var rules = {
     predicate: "was | is | had been | is now",
     future: "is now | will be | has a chance to be",
     status: "canceled | lost | postponed",
-    pos_status: "better | not as bad as before"
+    pos_status: "better | not as bad as before",
+    fire: "Wildfires rage across the countryside.",
+    heat: "A heatwave has shutdown nearby cities and towns.",
+    flood: "Flooding threatens low-lying areas.",
+    drought: "Drinking water levels are at an all-time low."
 }
