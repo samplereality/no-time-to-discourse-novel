d7cab3a8e09848d0ddf078da60b3b2485554668a
Mark Sample <samplereality@gmail.com>
2022-07-23 22:49:10 -0400

    Cleaned up js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 "use strict";
 
-// create base map
-// var map = L.map('map').fitWorld();
-
+// Load RiTa
 let rg = RiTa.grammar(rules); 
 
+// create base map
+// var map = L.map('map').fitWorld();
 
 var map = L.map('map', {
     center: [35.227, -80.8431],
@@ -30,79 +30,14 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{e
 	ext: 'jpg'
 }).addTo(map);
 
+// Grab coordinates from geojson, add stories to markers, and add to map
 $.getJSON('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_populated_places_simple.geojson', function(data) {
   L.geoJson(data, {
     style: function (feature) {
         return {color: feature.properties.color};
     }
 }).bindTooltip(function (layer) {
-    return layer.feature.properties.name + ", " + layer.feature.properties.ADM1NAME + "<br><br>" + rg.expand(); 
+    return layer.feature.properties.name + ", " + layer.feature.properties.adm1name + "<br><br>" + rg.expand(); 
 }, {opacity: 1.0, className: 'disasterLabels'}).addTo(map);
 
 });
-
-
-
-/**
- * Create an array of dreams
- */
-
-// function createDreams(n = 5) {
-//     // array to hold them
-//     let arr = [];
-//     // rg.load(grammar);
-//     // loop n times
-//     for (let i = 0; i < n; i++) {
-//         var results = rg.expand()
-//         arr.push(results);
-//     }
-//     return arr;
-// }
-
-/**
- * Create an array of fake users
- */
-// function createFakes(n = 5) {
-//     // array to hold them
-//     let arr = [];
-//     // loop n times
-//     for (let i = 0; i < n; i++) {
-//         arr.push(faker.helpers.userCard());
-//     }
-//     return arr;
-// }
-
-// array to hold markers and fakes
-// let markers = [],
-//     fakes = createFakes(200)
-//     var dreams = createDreams(200)
-//  console.log(fakes);
-//  console.log(dreams);
-
-// add markers to map 
-// addMarkers(fakes);
-
-/**
- * Add fake markers to map from fakes array 
- */
-// function addMarkers(fakes) {
-//     // for each fake 
-//     for (let i = 0; i < fakes.length; i++) {
-//         // create a marker with lat/lng
-//         let marker = L.marker([fakes[i].address.geo.lat, fakes[i].address.geo.lng]);
-//         // bind data into popup
-//         marker.bindPopup(dreams[i]);
-//         marker.on('mouseover', function(e) {
-//             this.openPopup();
-//         });
-//         marker.on('mouseout', function(e) {
-//             this.closePopup();
-//         });
-//         // add to marker array 
-//         markers.push(marker);
-//     }
-//     // create group from markers and add to map
-//     var group = L.featureGroup(markers).addTo(map);
-    // zoom to group
-    // map.fitBounds(group.getBounds());
-// }
