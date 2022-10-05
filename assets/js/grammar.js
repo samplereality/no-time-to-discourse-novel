0457284961f9d355dfddc1bff69e280bd8c0f701
Mark Sample <samplereality@gmail.com>
2022-10-04 20:10:59 -0400

    fixing rita

--- assets/js/grammar.js
+++ assets/js/grammar.js
@@ -12,7 +12,7 @@ var rules = {
     drought_time: "It's been (eight | nine | ten | eleven) months since rain. | It hasn't rained in (weeks and weeks | months | what feels like years).",
 
     fire: "$fireGeneral | $fireSpecific",
-    fireGeneral: "Wildfires rage (outside the city | across the countryside). (Thousands | Tens of thousands | Hundreds of thousands) of acres are destroyed. $person's job is to calculate the property damage. There's no way, $pronoun thinks.",
+    fireGeneral: "Wildfires rage (outside the city | across the countryside). (Thousands | Tens of thousands | Hundreds of thousands) of acres are destroyed. $fireJob",
     fireSpecific: "$fireHome $fireEnd",
     fireHome: "$person's (house | home | apartment | condo | townhouse) was (destroyed | burned to the ground | reduced to ashes | engulfed in flames). $pronoun.cap() remembers the most random things. $object.nr().art().cap(). $object.nr().art().cap(). $object.nr.cap().s(). All lost.",
     fireMemory: "$person remembers the most random things. $object.nr().art().cap(). $object.nr().art().cap(). $object.nr.cap().s(). All lost.",
